835e230f7841d38b93c3ef3de3f4df8a2b510e91
FlowerKnight55 <162504966+FlowerKnight55@users.noreply.github.com>
2024-08-09 03:29:41 -0400

    Fixed when pressing next round button human-image class duplicates

--- script.js
+++ script.js
@@ -4,39 +4,107 @@ let humanScore = 0;
 let computerScore = 0;
 
 
-function getHumanChoice(){
+let count = 0;
+// function getHumanChoice(){
     
     
+//     let choicebtn = document.querySelector(".choice-btn");
+    
+
+
+//     return new Promise((resolve) => {
+
+
+//         //choicebtn.removeEventListener("click");
+
+
+//         choicebtn.addEventListener("click", (event) =>{
+//             count++
+//             console.log(count);
+//             let target = event.target;
+//             let humanImageBorder = document.querySelector(".human-border");
+//             let humanImage = document.createElement("img");
+//             let humanCreditLink = document.createElement("a");
+//             let humanContainer = document.querySelector(".human-img-info");
+//             console.log(humanContainer);
+        
+//             humanImage.classList.add("human-illus");
+//             humanCreditLink.classList.add("human-credits")
+    
+        
+//             switch(target.id){
+//                 case "rock":
+//                     humanImage.setAttribute("src", "images/rock.png");
+//                     humanImageBorder.appendChild(humanImage);
+//                     humanCreditLink.setAttribute("href", "https://www.freepik.com/free-psd/hand-drawn-rock-formation_206468576.htm#fromView=search&page=1&position=23&uuid=b2c50176-522e-401b-9463-4ff61d1370ad");
+//                     humanCreditLink.textContent = "Image by freepik";
+//                     humanContainer.appendChild(humanCreditLink);
+    
+//                     break;
+//                 case "paper":
+//                     humanImage.setAttribute("src", "images/paper.png");
+//                     humanImageBorder.appendChild(humanImage);
+//                     humanCreditLink.setAttribute("href","https://www.freepik.com/free-vector/blank-vintage-scroll-paper_21302279.htm#fromView=search&page=1&position=30&uuid=e674b638-d1ec-4006-bd65-3594b94d8dad");
+//                     humanCreditLink.textContent = "Image by brgfx on Freepik";
+//                     humanContainer.appendChild(humanCreditLink);
+    
+//                     break;
+//                 case "scissors":
+//                     humanImage.setAttribute("src", "images/scissors.png");
+//                     humanImageBorder.appendChild(humanImage);
+//                     humanCreditLink.setAttribute("href", "https://www.freepik.com/free-vector/scissors-tool-icon_169503561.htm#fromView=search&page=2&position=0&uuid=a42698b7-905c-46c5-89eb-0984f3237f58");
+//                     humanCreditLink.textContent = "Image by freepik";
+//                     humanContainer.appendChild(humanCreditLink);
+    
+//                     break;
+//             }
+    
+//            resolve(target.id);
+            
+//         })
+        
+        
+//     });
+
+    
+
+// }
+
+
+function getHumanChoice() {
+
     let choicebtn = document.querySelector(".choice-btn");
-    
+
     return new Promise((resolve) => {
-        choicebtn.addEventListener("click", (event) =>{
+
+        // Define the event listener function separately
+        function handleClick(event) {
+            count++;
+            console.log(count);
             let target = event.target;
             let humanImageBorder = document.querySelector(".human-border");
             let humanImage = document.createElement("img");
             let humanCreditLink = document.createElement("a");
             let humanContainer = document.querySelector(".human-img-info");
-        
+            console.log(humanContainer);
+
             humanImage.classList.add("human-illus");
-            humanCreditLink.classList.add("human-credits")
-    
-        
-            switch(target.id){
+            humanCreditLink.classList.add("human-credits");
+
+            switch (target.id) {
                 case "rock":
                     humanImage.setAttribute("src", "images/rock.png");
                     humanImageBorder.appendChild(humanImage);
                     humanCreditLink.setAttribute("href", "https://www.freepik.com/free-psd/hand-drawn-rock-formation_206468576.htm#fromView=search&page=1&position=23&uuid=b2c50176-522e-401b-9463-4ff61d1370ad");
                     humanCreditLink.textContent = "Image by freepik";
                     humanContainer.appendChild(humanCreditLink);
-    
                     break;
                 case "paper":
                     humanImage.setAttribute("src", "images/paper.png");
                     humanImageBorder.appendChild(humanImage);
-                    humanCreditLink.setAttribute("href","https://www.freepik.com/free-vector/blank-vintage-scroll-paper_21302279.htm#fromView=search&page=1&position=30&uuid=e674b638-d1ec-4006-bd65-3594b94d8dad");
+                    humanCreditLink.setAttribute("href", "https://www.freepik.com/free-vector/blank-vintage-scroll-paper_21302279.htm#fromView=search&page=1&position=30&uuid=e674b638-d1ec-4006-bd65-3594b94d8dad");
                     humanCreditLink.textContent = "Image by brgfx on Freepik";
                     humanContainer.appendChild(humanCreditLink);
-    
                     break;
                 case "scissors":
                     humanImage.setAttribute("src", "images/scissors.png");
@@ -44,19 +112,29 @@ function getHumanChoice(){
                     humanCreditLink.setAttribute("href", "https://www.freepik.com/free-vector/scissors-tool-icon_169503561.htm#fromView=search&page=2&position=0&uuid=a42698b7-905c-46c5-89eb-0984f3237f58");
                     humanCreditLink.textContent = "Image by freepik";
                     humanContainer.appendChild(humanCreditLink);
-    
                     break;
             }
-    
-           resolve(target.id);
-            
-        })
-        
+
+            resolve(target.id);
+
+            // Remove the event listener after the first click
+            choicebtn.removeEventListener("click", handleClick);
+        }
+
+        // Attach the event listener
+        choicebtn.addEventListener("click", handleClick);
+
     });
 
-    
-
 }
+
+
+
+
+
+
+
+
 
 
 function getComputerChoice(){
@@ -112,12 +190,17 @@ function getComputerChoice(){
 }
 
 
-async function enterChoice(){
-    let humanChoice = await getHumanChoice();
-    let computerChoice = await getComputerChoice();
+// async function enterChoice(){
 
-    await playRound(humanChoice, computerChoice);
-}
+//     return new Promise((resolve) =>{
+        
+//     })
+
+//     let humanChoice = await getHumanChoice();
+//     let computerChoice = await getComputerChoice();
+
+//     await playRound(humanChoice, computerChoice);
+// }
 
 
 //Plays a round of the game after finish a game on to the next round.
@@ -128,8 +211,7 @@ function playRound(humanChoice, computerChoice){
     const computerScoreboard = document.querySelector(".computer-scoreboard");
 
     return new Promise((resolve) =>{
-        console.log("Round " + (round));
-        console.log(humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase() + " vs " + computerChoice);
+        
         //Decides who wins the round
         if(humanChoice.toLowerCase() === computerChoice.toLowerCase()){
             winnerText.textContent = "It's a draw!"
@@ -160,7 +242,7 @@ function playRound(humanChoice, computerChoice){
 }
 
 
-function getNextRound(callback){
+function getNextRound(){
 
     return new Promise((resolve) =>{
         const proceedSection = document.querySelector(".proceed-section");
@@ -203,6 +285,7 @@ function getNextRound(callback){
                 roundText.textContent = "Round: " + round;
 
             })
+            resolve();
 
         }
         else{
@@ -214,9 +297,7 @@ function getNextRound(callback){
 
         }
 
-
-
-
+        
     })
 
 
@@ -224,14 +305,7 @@ function getNextRound(callback){
 
 
 
-//Play a game for 5 rounds
-async function playGame(callback, currentRound){
-    while(round <= currentRound){
-        await enterChoice();
-        await getNextRound();
-    }
-    callback();
-}
+
 
 function getFinalScore(){
     console.log("Final Score: ");
@@ -245,8 +319,22 @@ function getFinalScore(){
     }
 }
 
+//Play a game for 5 rounds
+async function playGame(currentRound){
+    while(round <= currentRound){
+        let humanChoice = await getHumanChoice();
+        let computerChoice = await getComputerChoice();
 
-playGame(getFinalScore, NUMBEROFGAMES);
+        await playRound(humanChoice, computerChoice);
+        await getNextRound();
+    }
+    await getFinalScore();
+}
+
+
+
+
+playGame(NUMBEROFGAMES);
 
 
 
